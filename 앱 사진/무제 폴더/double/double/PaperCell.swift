62648d3708cbeebf4df082a35176ff3241ff6973
purification <cscoi007@cscoi007ui-iMac.local>
2017-02-08 17:26:08 +0900

    Double2

---
+++ 앱 사진/무제 폴더/double/double/PaperCell.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  double
+//
+//  Created by cscoi007 on 2017. 2. 3..
+//  Copyright © 2017년 cscoi007. All rights reserved.
+//
+
+import UIKit
+
+class PaperCell : UITableViewCell   {
+    
+    @IBOutlet var Title: UILabel!
+    
+    @IBOutlet var Author: UILabel!
+    
+}
